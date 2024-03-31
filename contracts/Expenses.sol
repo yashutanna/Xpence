4c5432b11877373249542d23ba127d7779f540ed
yashutanna <tanna.yashu+remix@gmail.com>
2024-03-31 23:48:11 +0200

    re-organise into debits and credit with simplification

--- contracts/Expenses.sol
+++ contracts/Expenses.sol
@@ -3,32 +3,22 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Expenses {
-    struct Expense {
-        uint expenseId;
+    event NewCredit(address creditor, address asset, uint amount);
+    event NewDebit(address debtor, address asset, uint amount);
+
+    struct LedgerEntry {
         uint amount;
-        address token;
+        address asset;
+        string description;
     }
 
-    struct Income {
-        uint incomeId;
-        uint expenseId;
-        uint amount;
-        address token;
+    struct Ledger {
+        LedgerEntry[] debits;
+        LedgerEntry[] credits;
     }
 
-    event NewExpense(address creditor, uint expenseId);
-    event NewIncome(address debtor, uint incomeId);
-
-    uint public expenseIndex;
-
-    mapping(address => uint) public participantIncomesIds;
+    mapping(address => Ledger) private participantLedger;
     mapping(address => bool) public isParticipant;
-
-    mapping(address => Expense[]) public expensesByParticipant;
-    mapping(address => Income[]) public incomesByParticipants;
-    mapping(uint => Expense) public expensesByExpenseId;
-    mapping(address => mapping(uint => Income)) public participantIncomeByIncomeId;
-    mapping(address => mapping(address => Income[])) public participantIncomesByAsset;
 
     modifier onlyParticipant {
         require(isParticipant[msg.sender] == true);
@@ -51,37 +41,24 @@ contract Expenses {
         }
     }
 
-    function addExpenseSimple(address _creditor, uint _amount, address _asset, address[] memory _debtors) external onlyParticipant {
+    function addCreditSimple(address _creditor, uint _amount, address _asset, address[] memory _debtors, string memory _description) external onlyParticipant {
         checkDebtors(_debtors);
-        uint expenseId = expenseIndex++;
-        Expense memory expense = Expense(expenseId, _amount, _asset);
-        
-        expensesByExpenseId[expenseId] = expense;
-        expensesByParticipant[_creditor].push(expense);
+        LedgerEntry memory debit = LedgerEntry(_amount, _asset, _description);
+        participantLedger[_creditor].debits.push(debit);
+        emit NewCredit(_creditor, _asset, _amount);
 
         for(uint i = 0; i < _debtors.length; i++){
-            uint participantIncomeId = participantIncomesIds[_debtors[i]]++;
-            Income memory income = Income(participantIncomeId, expenseId, _amount/_debtors.length, _asset);
-            incomesByParticipants[_debtors[i]].push(income);
-            participantIncomeByIncomeId[_creditor][participantIncomeId] = income;
-            participantIncomesByAsset[_creditor][_asset].push(income);
+            LedgerEntry memory credit = LedgerEntry(_amount/_debtors.length, _asset, _description);
+            participantLedger[_debtors[i]].credits.push(credit);
+            emit NewDebit(_debtors[i], credit.asset, credit.amount);
         }
     }
 
-    function listExpenses(address _participant) external view returns (Expense[] memory) {
-        return expensesByParticipant[_participant];
+    function listCredits(address _participant) external view returns (LedgerEntry[] memory) {
+        return participantLedger[_participant].credits;
     }
 
-    function listIncomes(address _participant) external view returns (Income[] memory) {
-        return incomesByParticipants[_participant];
+    function listDebits(address _participant) external view returns (LedgerEntry[] memory) {
+        return participantLedger[_participant].debits;
     }
-
-    function listIncomes(address _participant, address _asset) external view returns (Income[] memory) {
-        return participantIncomesByAsset[_participant][_asset];
-    }
-
-    function getIncome(address _participant, uint incomeId) external view returns (Income memory) {
-        return participantIncomeByIncomeId[_participant][incomeId];
-    }
-
 }
